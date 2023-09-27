25a74462d55b12571cd0695e85f018d4b0fba2c8
Lyam0udi <lyamoudi.ilias77@gmail.com>
2023-09-27 01:27:05 +0100

    Updates

--- src/scientificCalculatorPack/CalculatorHistory.java
+++
@@ -1,6 +0,0 @@
-package scientificCalculatorPack;
-
-// This class could handle keeping a history of calculations or user interactions, allowing users to review their calculations.
-public class CalculatorHistory {
-
-}
--- src/scientificCalculatorPack/CalculatorMemory.java
+++
@@ -1,6 +0,0 @@
-package scientificCalculatorPack;
-
-// This class might manage memory-related operations, such as storing and retrieving values from memory registers.
-public class CalculatorMemory {
-
-}
